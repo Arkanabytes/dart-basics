b12da3b5618a0bcca9854cf701a2c219f0d460bb
Alejandra Pinto <copinto-@student.42.us.org>
2020-07-10 21:29:47 +0200

    .

--- 12-mixins.dart
+++ 12-mixins.dart
@@ -19,27 +19,18 @@ abstract class Nadador {
   void nadar() => print('Estoy nadando!!');
 }
 
-
 class Delfin extends Mamifero with Nadador {}
-
 class Murcielago extends Mamifero with Caminante, Volador {}
-
 class Gato extends Mamifero with Caminante {}
-
 class Paloma extends Ave with Caminante, Volador {}
-
 class Pato extends Ave with Caminante, Volador, Nadador {}
-
 class Tiburon extends Pez with Nadador {}
-
 class PezVolador extends Pez with Nadador, Volador { }
-
 
 void main() {
   
   final pato = new Pato();
   pato.volar();
-  
   final pezVolador = new PezVolador();
   pezVolador.nadar();
   
